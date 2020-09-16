411585232b8ff17fb16815eac75541b6c28a3dcf
Victor Allen <vamuigua@gmail.com>
2020-09-17 00:05:34 +0300

    Added Destroy action for Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -53,7 +53,6 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
     }
 
     /**
@@ -87,6 +86,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post);
+        return redirect('/posts');
     }
 }
